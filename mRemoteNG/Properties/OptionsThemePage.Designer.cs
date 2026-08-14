8ddbfaa46445e83b33890b035aca312dec4eb183
local <local@localhost>
2026-08-14 15:28:29 +0200

    Default to the vs2015darkNG theme
    ThemeName defaulted to "vs2015Light", which is not even a key that
    LoadThemes() produces for a file based theme, so a fresh profile fell back
    to the built-in light theme. Point it at vs2015darkNG, turn theming on and
    mark the active theme dark so the common controls are painted dark from
    the first startup instead of flashing light.

--- mRemoteNG/Properties/OptionsThemePage.Designer.cs
+++ mRemoteNG/Properties/OptionsThemePage.Designer.cs
@@ -25,7 +25,7 @@ namespace mRemoteNG.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool ThemingActive {
             get {
                 return ((bool)(this["ThemingActive"]));
@@ -37,7 +37,7 @@ namespace mRemoteNG.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("vs2015Light")]
+        [global::System.Configuration.DefaultSettingValueAttribute("vs2015darkNG")]
         public string ThemeName {
             get {
                 return ((string)(this["ThemeName"]));
@@ -61,7 +61,7 @@ namespace mRemoteNG.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IsActiveThemeDark {
             get {
                 return ((bool)(this["IsActiveThemeDark"]));
